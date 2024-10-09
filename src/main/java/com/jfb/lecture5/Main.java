7ed44e356aaf2b1e8ab2403a78bce421a18926d5
Vitali Kalaskou <kolosokvit@gmail.com>
2024-10-09 23:43:10 +0300

    Complete HW-5

--- src/main/java/com/jfb/lecture5/Main.java
+++ src/main/java/com/jfb/lecture5/Main.java
@@ -6,6 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jfb.lecture5.model.BusTicket;
 import com.jfb.lecture5.model.BusTicketValidator;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -34,17 +39,66 @@ public class Main {
         System.out.println("Invalid input, ticket can't be created");
       }
     } while (x < 5);
-    
     System.out.println("Total = " + totalTickets);
     System.out.println("Valid = " + validTickets);
     if (startDateViolations > priceViolations) {
       System.out.println("Most popular violation = startDate");
-    } else {
+    } else if (startDateViolations < priceViolations) {
       System.out.println("Most popular violation = price");
+    } else {
+      System.out.println("Most popular violation = none");
+    }
+
+    //Reading tickets from file and validating
+    validTickets = 0;
+    startDateViolations = 0;
+    priceViolations = 0;
+    int typeViolations = 0;
+    List<BusTicket> busTickets = readTicketsFromFile("src/main/resources/ticketData.txt");
+    totalTickets = busTickets.size();
+    for (BusTicket busTicket : busTickets) {
+      if (!BusTicketValidator.isValidByStartDate(busTicket)) {
+        startDateViolations++;
+      } else if (!BusTicketValidator.isValidByType(busTicket)) {
+        typeViolations++;
+      } else if (!BusTicketValidator.isPriceEven(busTicket)) {
+        priceViolations++;
+      } else {
+        validTickets++;
+      }
+    }
+    System.out.println("Total = " + totalTickets);
+    System.out.println("Valid = " + validTickets);
+    int maxNumberOfViolations = Math.max(startDateViolations, Math.max(priceViolations, typeViolations));
+    if (startDateViolations == maxNumberOfViolations) {
+      System.out.println("Most popular violation = startDate");
+    } else if (priceViolations == maxNumberOfViolations){
+      System.out.println("Most popular violation = price");
+    } else {
+      System.out.println("Most popular violation = type");
     }
   }
 
   private static String getInput() {
     return new Scanner(System.in).nextLine();
+  }
+
+  private static List<BusTicket> readTicketsFromFile(String file) {
+    List<BusTicket> tickets = new ArrayList<>();
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+      String input = reader.readLine();
+      while (input != null) {
+        try {
+          tickets.add(new ObjectMapper().readValue(input, BusTicket.class));
+          input = reader.readLine();
+        } catch (JsonParseException e) {
+          System.out.println("Invalid input, ticket can't be created");
+          input = reader.readLine();
+        }
+      }
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+    return tickets;
   }
 }
